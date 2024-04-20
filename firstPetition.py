444ae34d2a0efefe2c3e320039dfc4be4649a31b
Santiago Forero Yate <santiago.forero-y@mail.escuelaing.edu.co>
2024-04-19 20:10:21 -0500

    estrucutra base lab 9, falta corregir error con API key

---
+++ firstPetition.py
@@ -0,0 +1,22 @@
+from langchain.chains import LLMChain
+from langchain_community.llms import OpenAI
+from langchain.prompts import PromptTemplate
+
+import os
+
+os.environ["OPENAI_API_KEY"] = "sk-proj-Hc8d6YYvL622muzmCnOvT3BlbkFJiJPiJDhzjS5J36vFQcaj"
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm = OpenAI()
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+question = "dime lenguajes de programación mas usados en el desarrollo de software"
+
+response = llm_chain.run(question)
+print(response)
